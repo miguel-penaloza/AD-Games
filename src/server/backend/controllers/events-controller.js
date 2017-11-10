a6da861f0725d8d2e69232b073709df76ae6d3e4
Jorge Rodriguez <jorge.rodriguez@appdirect.com>
2017-11-10 19:22:38 -0300

    Object to array

--- src/server/backend/controllers/events-controller.js
+++ src/server/backend/controllers/events-controller.js
@@ -6,6 +6,7 @@ class EventsController extends Controller {
 		super();
 		this.router.get('/events', this.checkLogin, this.getEvents.bind(this));
 		this.router.get('/events/:id', this.checkLogin, this.getEventById.bind(this));
+		this.router.post('/events', this.checkLogin, this.create.bind(this));
 		//this.router.get('/events/vote', this.getEventById.bind(this));
 	}
 
@@ -15,21 +16,21 @@ class EventsController extends Controller {
 
 	getEventById(req, res) {
 		try {
-			var id = parseInt(req.params.id);
-			if (isNaN(id)) {
-				res.sendStatus(400);
-			} else {
-				Event.getInstace(this.db).getById(id).then((val) => {
-					if (val == null) {
-						res.sendStatus(404);
-					} else {
-						res.send(val);
-					}
-				}).catch((error) => res.sendStatus(500));
-			}
+			Event.getInstace(this.db).getById(req.params.id).then((val) => {
+				if (val == null) {
+					res.sendStatus(404);
+				} else {
+					res.send(val);
+				}
+			}).catch((error) => res.sendStatus(500));
 		} catch (e) {
+			console.log();
 			res.sendStatus(400);
 		}
+	}
+
+	create(req, res) {
+		Event.getInstace(this.db).create({id: 10}).then((val) => res.send(val)).catch((error) => res.send([]));
 	}
 
 }
